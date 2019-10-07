83ccac767b3716ca6ea81ba50b407c19dcf9f846
Victoria-Hodder <morethangrammar@gmail.com>
2019-10-07 20:35:17 +0200

    added front-end

---
+++ public_profiles/static/public_profiles/app.js
@@ -0,0 +1,12 @@
+$('.message a').click(function(){
+    $('form').animate({height:"toggle", opacity:"toggle"},"slow");
+    });
+    // $('#submit-button').on("click" , function(e){
+        // e.preventDefault();
+    //     const username = $("#username").val();
+    //     const whatever = $("#password").val();
+    //     $.post( "ajax/test.html", function( data ) {
+    //         $( ".result" ).html( data );
+    //       });
+
+    // })
