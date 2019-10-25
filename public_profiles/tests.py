de1d890152e801d6ad361f5ece558ec6a66f5633
Victoria-Hodder <morethangrammar@gmail.com>
2019-10-25 11:15:17 +0200

    added tests to check root url and home html

--- public_profiles/tests.py
+++ public_profiles/tests.py
@@ -1,3 +1,15 @@
+from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 
-# Create your tests here.
+from public_profiles.views import home
+
+class HomePageTest(TestCase):
+
+    def test_root_url_returns_home_view(self):
+        found = resolve('/')  
+        self.assertEqual(found.func, home)
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'public_profiles/home.html', 'public_profiles/base.html')
--- public_profiles/views.py
+++ public_profiles/views.py
@@ -2,21 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 
-profiles = [                                    # dummy data for landing site
-    {
-        'Username': 'Superduck',
-        'Skills': 'Python, Django, Flask',
-        'Dislikes': 'meat, ignorance',
-        'Availablity': 'Starting from January 2020'
-    },
-    {
-        'Username': 'Wonderspider',
-        'Skills': 'Javascript, Bootstrap, HTML',
-        'Dislikes': 'Antisemitism',
-        'Availablity': 'Starting from February 2020'
-    }
-]
-
 def home(request):
     context = {
         'profiles': User.objects.all()
