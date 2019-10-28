d471c7712fa952658cbb7334e24219c4eba9c8bf
Victoria-Hodder <morethangrammar@gmail.com>
2019-10-28 20:09:00 +0100

    added functional_test.py file

---
+++ functional_tests.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
