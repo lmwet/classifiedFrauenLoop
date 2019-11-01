2e6cc196d4909aee962f3d736b76a5d84302242d
Victoria-Hodder <morethangrammar@gmail.com>
2019-11-01 12:09:18 +0100

    wrote user journey to guide later functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -12,11 +12,19 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_frauenlooper_can_access_classified(self):
-        #Ms X found out about Frauenloop classified and wants to check it out
+        # Ms X found out about Frauenloop classified and wants to check it out
         self.browser.get('http://localhost:8000')
         self.assertIn('Frauenloop', self.browser.title)  
         self.fail('Finish the test!')
 
+        # Ms X thinks it's a super cool idea and decides she must set up an account immediately
+        # She goes to the register page and fills in her details
+        # She clicks submit and the page returns her to the login page
+        # She's so excited she decides to complete her profile straight away so she logs in
+        # Ms X navigates to her profile page and completes her profile.
+        # She saves the information and logs out because she has an important appointment in 5 minutes
+        # Later she comes back and wants to edit her bio, so she logs back in
+        # She edits her bio then goes to bed
 
 
     if __name__ == '__main__':  
