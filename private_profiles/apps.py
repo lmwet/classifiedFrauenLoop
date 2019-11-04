b0eb28adafd2ffe55fbe44ff5caa03c0266e12d7
Victoria-Hodder <morethangrammar@gmail.com>
2019-11-04 20:50:41 +0100

    created Profile model and started to build profile page

--- private_profiles/apps.py
+++ private_profiles/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class PrivateProfilesConfig(AppConfig):
     name = 'private_profiles'
+
+    def ready(self):
+        import private_profiles.signals
--- private_profiles/forms.py
+++ private_profiles/forms.py
@@ -1,20 +1,22 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
 
 class ClassifiedRegistrationForm(UserCreationForm):
     username = forms.CharField(label='', 
                     widget=forms.TextInput(attrs={'placeholder': 'Username'}))  # creating fields in addition to in-built User fields 
     email = forms.EmailField(label='', 
                     widget=forms.TextInput(attrs={'placeholder': 'Email'}))     # all fields habe a placeholder, label (above field) remains empty
-    skills = forms.CharField(label='', 
-                    widget=forms.TextInput(attrs={'placeholder': 'Skills'}))
-    experience = forms.CharField(label='', 
-                    widget=forms.TextInput(attrs={'placeholder': 'Experience'}))
-    dislikes = forms.CharField(label='', 
-                    widget=forms.TextInput(attrs={'placeholder': 'What I dislike'}))
-    availability = forms.CharField(label='', 
-                    widget=forms.TextInput(attrs={'placeholder': 'Availability'}))
+    # skills = forms.CharField(label='', 
+    #                 widget=forms.TextInput(attrs={'placeholder': 'Skills'}))
+    # experience = forms.CharField(label='', 
+    #                 widget=forms.TextInput(attrs={'placeholder': 'Experience'}))
+    # dislikes = forms.CharField(label='', 
+    #                 widget=forms.TextInput(attrs={'placeholder': 'What I dislike'}))
+    # availability = forms.CharField(label='', 
+    #                 widget=forms.TextInput(attrs={'placeholder': 'Availability'}))
     password1 = forms.CharField(label='', 
                     widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
     password2 = forms.CharField(label='', 
@@ -22,4 +24,22 @@ class ClassifiedRegistrationForm(UserCreationForm):
 
     class Meta:
         model = User # our model is based on django's User model
-        fields = ['username', 'email', 'skills', 'experience', 'dislikes', 'availability', 'password1', 'password2'] # succession of fields in order of appearance on site
+        fields = ['username', 'email', 'password1', 'password2'] # succession of fields in order of appearance on site
+
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta():
+        model = User
+        fields = ['username', 'email']
+
+
+# import Profile (see above)
+
+class ProfileUpdateForm(forms.ModelForm):
+
+    class Meta():
+        model = Profile
+        fields = ['bio', 'image']
