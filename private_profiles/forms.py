647c242dd7064610bd45b8329c0e7ef47596e0d3
Victoria-Hodder <morethangrammar@gmail.com>
2019-11-07 12:10:20 +0100

    updated views.py

--- private_profiles/forms.py
+++ private_profiles/forms.py
@@ -41,11 +41,3 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta():
         model = Profile
         fields = ['skills', 'languages', 'likes', 'dislikes', 'github', 'image']
-
-
-
-        # skills = models.CharField(max_length=250, blank=True)
-    # languages = models.CharField(max_length=250, blank=True)
-    # likes = models.CharField(max_length=250, blank=True)
-    # dislikes = models.CharField(max_length=250, blank=True)
-    # github
