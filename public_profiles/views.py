e47636eb546c5939df009eba8c445a027c9069b8
Victoria-Hodder <morethangrammar@gmail.com>
2019-12-05 15:16:36 +0100

    added post function, tested and it sends an email to the system, yay!

--- public_profiles/views.py
+++ public_profiles/views.py
@@ -30,23 +30,20 @@ class ProfileDetailView(DetailView):
         context['form'] = form
         return self.render_to_response(context)
 
+    def post(self, request, pk):
+        form = ContactForm(request.POST)
 
-    # def emailView(self, request, pk):
-    #     if request.method == 'GET':
-    #         form = ContactForm()
-    #     else:
-    #         form = ContactForm(request.POST)
-    #         if form.is_valid():
-    #             subject = form.cleaned_data['subject']
-    #             from_email = form.cleaned_data['from_email']
-    #             message = form.cleaned_data['message']
-    #             try:
-    #                 m_send = EmailMessage(subject, message, from_email, ['admin@example.com'], cc=['riekeland@gmail.com'], bcc=['riekeland@compnay.com'])
-    #                 m_send.send()
-    #             except BadHeaderError:
-    #                 return HttpResponse('Invalid header found.')
-    #             return redirect('success')
-    #     return render(request, "public_profiles/profile_details.html", {'form': form})
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                m_send = EmailMessage(subject, message, from_email, ['admin@example.com'], cc=['riekeland@gmail.com'], bcc=['riekeland@compnay.com'])
+                m_send.send()
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success')
+        return render(request, "public_profiles/profile_details.html", {'form': form})
 
 def about(request):
     return render(request, 'public_profiles/about.html', {'title': 'About'})
